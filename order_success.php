170253283751e6eb27d5c81e889bac439f78567c
kasiaskrz <114486282+kasiaskrz@users.noreply.github.com>
2025-04-28 17:31:30 +0100

    Add files via upload

---
+++ order_success.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Pocket Pet Tailor</title>
+    <link rel="icon" href="images/favicon.png" type="image/png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Londrina+Sketch&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+    
+    <div class="card text-center">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="true" href="#">Shop Online</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reviewsPage.php">Reviews</a>
+                </li>
+                <li>
+                    <button onclick="window.location.href='logout.php'" class="btn btn-danger">Logout</button>
+                </li>
+            </ul>
+            <img src="images/Logo.png" width="400px">
+        </div>
+        <div class="card-body">
+
+
+        </div>
+        <h2>Thank you for your order!</h2>
+    </div>
+    <h1></h1>
+    <a href="home.php" id='continue-shopping-link'>Continue Shopping</a>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</body>
+
+</html>
